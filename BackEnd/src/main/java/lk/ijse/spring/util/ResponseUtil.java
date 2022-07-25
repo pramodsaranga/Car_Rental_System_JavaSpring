542226ee42274992e1f8a3de5bbd745e9e4125be
pramodsaranga <pramodsaranga31@gmail.com>
2022-07-26 00:00:52 +0530

    Updated  Context

---
+++ BackEnd/src/main/java/lk/ijse/spring/util/ResponseUtil.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+
+    private int code;
+    private String massage;
+    private Object data;
+}
